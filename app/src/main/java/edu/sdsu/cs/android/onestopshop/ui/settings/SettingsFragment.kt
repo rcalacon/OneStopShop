c5e4e06955dd50759ccaea38f07fccdeeb7fba5d
Rodney Alacon <rcalacon@hotmail.com>
2020-05-08 17:02:25 -0700

    added reset button to settings to reset grocery data. still need to read from file and include everything

--- app/src/main/java/edu/sdsu/cs/android/onestopshop/ui/settings/SettingsFragment.kt
+++ app/src/main/java/edu/sdsu/cs/android/onestopshop/ui/settings/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package edu.sdsu.cs.android.onestopshop.ui.settings
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,11 +9,16 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import edu.sdsu.cs.android.onestopshop.R
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment() {
 
     private lateinit var settingsViewModel: SettingsViewModel
+    // Access a Cloud Firestore instance from your Activity
+    val db = Firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,5 +33,22 @@ class SettingsFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        reset_button.setOnClickListener {
+            val grocery = hashMapOf(
+                "name" to "eggs"
+            )
+            db.collection("groceries")
+                .add(grocery)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("RCA", "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("RCA", "Error adding document", e)
+                }
+        }
     }
 }
