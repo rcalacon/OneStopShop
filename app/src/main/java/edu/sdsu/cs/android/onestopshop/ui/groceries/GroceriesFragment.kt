7ee82d8826dd8e815ec2d465f3f722b64710ae14
Rodney Alacon <rcalacon@hotmail.com>
2020-05-10 11:43:30 -0700

    created ui for grocery fragment. still need to create recyclerview

--- app/src/main/java/edu/sdsu/cs/android/onestopshop/ui/groceries/GroceriesFragment.kt
+++ app/src/main/java/edu/sdsu/cs/android/onestopshop/ui/groceries/GroceriesFragment.kt
@@ -1,18 +1,25 @@
 package edu.sdsu.cs.android.onestopshop.ui.groceries
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import edu.sdsu.cs.android.onestopshop.R
+import edu.sdsu.cs.android.onestopshop.UserListActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class GroceriesFragment : Fragment() {
 
     private lateinit var groceriesViewModel: GroceriesViewModel
+    private val db = Firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,5 +34,27 @@ class GroceriesFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initializeGroceryList()
+    }
+
+    private fun initializeGroceryList(){
+        db.collection("groceries")
+            .get()
+            .addOnSuccessListener { result ->
+                for (grocery in result) {
+                    val currentGroceryData = grocery.data
+                    val groceryId = currentGroceryData["id"]
+                    val groceryName = currentGroceryData["name"]
+                    //place data into recyclerview
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("RCA", "Error getting documents.", exception)
+                login_progress.visibility = View.INVISIBLE
+            }
     }
 }
